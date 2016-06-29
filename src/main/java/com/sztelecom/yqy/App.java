f76f10622550493820d39f531a7544f5253773e1
manyi100 <manyi100@gmail.com>
2016-06-29 20:05:52 +0800

    create project

---
+++ src/main/java/com/sztelecom/yqy/App.java
@@ -0,0 +1,13 @@
+package com.sztelecom.yqy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
